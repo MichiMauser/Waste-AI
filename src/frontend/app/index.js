4dcbda924a68212c7f4bcb8ee8787dfb7476061c
Sampalean-Sorin <sampaleansorin@outlook.com>
2023-11-04 11:48:48 +0200

    First frontend commit

---
+++ src/frontend/app/index.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+
+const index = () => {
+  return (
+    <View>
+      <Text>index</Text>
+
+      <Link href ="/home">Go home</Link>
+        <Link href ="/profile">Go profile</Link>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
