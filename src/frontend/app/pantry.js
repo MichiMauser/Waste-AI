33594074f05741accd7a3134a9e6448672dda975
Tosa Cezar <67374801+CZew2003@users.noreply.github.com>
2023-11-04 21:20:12 +0200

    second commit, am modificat appBarPantry.js si pantry.js

--- src/frontend/app/pantry.js
+++ src/frontend/app/pantry.js
@@ -1,12 +1,14 @@
-import * as React from 'react';
-import { View, SafeAreaView, ImageBackground, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, SafeAreaView, ImageBackground, TouchableOpacity, Text, Modal } from 'react-native';
 import ProductText from './productText.js';
+import AppBarPantry from './appBarPantry.js'
+import { Link, Redirect } from 'expo-router'
 
 const PantryScreen = () => {
+
     return (
         <SafeAreaView>
-            
-            
+            <AppBarPantry></AppBarPantry>
             <View>
                 <ProductText productName={"Ana"} type={1} expirationDate={"2023-01-01"}></ProductText>
                 <ProductText productName={"Ana"} type={2} expirationDate={"2123"}></ProductText>
