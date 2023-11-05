533b2a256f92e91e52be711b2c9d6d824701998f
Sampalean-Sorin <sampaleansorin@outlook.com>
2023-11-05 15:28:14 +0200

    works

--- src/frontend/app/productText.js
+++ src/frontend/app/productText.js
@@ -14,6 +14,7 @@ const ProductText = ({ productName, type, expirationDate }) => {
         borderColor = '#A85353';
     }
   return (
+    
         <View style={{marginVertical: 15,
         borderWidth:3, marginHorizontal:30, paddingVertical:10,
         borderRadius:30, borderColor: borderColor}}> 
@@ -23,7 +24,20 @@ const ProductText = ({ productName, type, expirationDate }) => {
                     <Text>{expirationDate}</Text>
                 </View>
             </Link>
+            <View style={{ padding: 20 }}>
+        {/* <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Products:</Text> */}
+        {/* {products.map((product, index) => (
+          <View key={index} style={{ marginVertical: 10 }}>
+            <Text>Date of Expiry: {product.date_of_expiry}</Text>
+            <Text>Item ID: {product.item_id}</Text>
+            <Text>Notes: {product.notes || 'None'}</Text>
+            <Text>Owner ID: {product.owner_id}</Text>
+            <Text>Product Name: {product.product_name || 'N/A'}</Text>
+          </View>
+        ))} */}
+      </View>
         </View>
+        
   );
 };
 
