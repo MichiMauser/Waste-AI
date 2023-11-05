8a2b4989b696001d4e9d3e81c77ae6d86a4b059e
Sampalean-Sorin <sampaleansorin@outlook.com>
2023-11-05 16:26:44 +0200

    fixed

--- src/frontend/app/appBarPantry.js
+++ src/frontend/app/appBarPantry.js
@@ -37,7 +37,7 @@ const AppBarPantry = () => {
         },
       });
       console.log(response.data);
-      set(response.data);
+      setProducts(response.data);
     } catch (error) {
       console.error("Error fetching products:", error);
     }
