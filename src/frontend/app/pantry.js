bccb83b1b1f22dbe78f76066dcd17a1928c187a2
Sampalean-Sorin <sampaleansorin@outlook.com>
2023-11-05 15:32:23 +0200

    now is better

--- src/frontend/app/pantry.js
+++ src/frontend/app/pantry.js
@@ -11,10 +11,7 @@ const PantryScreen = () => {
                 <AppBarPantry></AppBarPantry>
             </View>
             <View>
-                <ProductText productName={"Ana"} type={1} expirationDate={"2023-01-01"}></ProductText>
-                <ProductText productName={"Ana"} type={2} expirationDate={"2123"}></ProductText>
-                <ProductText productName={"Ana"} type={3} expirationDate={"2123"}></ProductText>
-                <ProductText productName={"Ana"} type={4} expirationDate={"2123"}></ProductText>
+                
             </View>
         </SafeAreaView>
     )
